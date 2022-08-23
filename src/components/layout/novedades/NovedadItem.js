1c9a00f38f45318128e7a91f8279620253067b85
Joaquin Inamine Garcia <joaquingarcia279@gmail.com>
2022-08-23 16:38:28 -0300

    23/08/2022

--- src/components/layout/novedades/NovedadItem.js
+++ src/components/layout/novedades/NovedadItem.js
@@ -4,13 +4,12 @@ const NovedadItem = (props) => {
     const { title, subtitle, imagen, body } = props;
 
     return {
-        < div className = "novedades" > 
+        <div className = "novedades" > 
+
         <h1>{title}</h1>
         <h2>{subtitle}</h2>
         <img src={imagen} />
-        <div dangerouslySetInnerHTML={{__html: body }}/>
-        <hr />
-        
+        <div dangerouslySetInnerHTML={{ __html: body }}/>
         
         </div >
     };
